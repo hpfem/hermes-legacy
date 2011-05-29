4cdf57975aad733c3cbca3fd7f867363430f5579
Ma Zhonghua <mazhonghua83@gmail.com>
2011-05-29 21:07:34 +0800

    H2D: splitting of definitions.cpp for benchmarks-general/lshape.

---
+++ hermes2d/benchmarks-general/lshape/definitions.h
@@ -0,0 +1,15 @@
+#include "hermes2d.h"
+
+/*  Exact solution */
+
+class CustomExactSolution : public ExactSolutionScalar
+{
+public:
+  CustomExactSolution(Mesh* mesh) : ExactSolutionScalar(mesh) {};
+
+  virtual scalar value (double x, double y) const;
+
+  virtual void derivatives (double x, double y, scalar& dx, scalar& dy) const;
+
+  virtual Ord ord(Ord x, Ord y) const;
+};
