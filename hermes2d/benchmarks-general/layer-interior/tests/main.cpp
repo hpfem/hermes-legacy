38d2b083db6c456ec86fec4973c33a666ba23a9a
Ma Zhonghua <mazhonghua83@gmail.com>
2011-05-29 20:42:55 +0800

    H2D: splitting of definitions.cpp for benchmarks-general/layer-interior.

--- hermes2d/benchmarks-general/layer-interior/tests/main.cpp
+++ hermes2d/benchmarks-general/layer-interior/tests/main.cpp
@@ -1,6 +1,6 @@
 #define HERMES_REPORT_ALL
 #define HERMES_REPORT_FILE "application.log"
-#include "hermes2d.h"
+#include "../definitions.h"
 
 using namespace RefinementSelectors;
 
@@ -60,9 +60,6 @@ MatrixSolverType matrix_solver = SOLVER_UMFPACK;  // Possibilities: SOLVER_AMESO
 
 // Problem parameters.
 double slope = 60;                                // Slope of the layer.
-
-// Right-hand side, exact solution, weak forms.
-#include "../definitions.cpp"
 
 int main(int argc, char* argv[])
 {
