c54980c5a53827a91765487bf1cff92ff6ba0652
Melanie Bittl <melli282@gmx.de>
2011-06-06 10:09:13 +0200

    CSCMatrix::multiply_with_vector changed

--- hermes_common/solver/umfpack_solver.cpp
+++ hermes_common/solver/umfpack_solver.cpp
@@ -84,7 +84,7 @@ void CSCMatrix::multiply_with_vector(scalar* vector_in, scalar* vector_out)
   for (int j=0; j<n; j++) vector_out[j] = 0;
   for (int j=0; j<n; j++) {
     for (int i = Ap[j]; i < Ap[j + 1]; i++) {
-      vector_out[j] += vector_in[Ai[i]]*Ax[i];
+      vector_out[Ai[i]] += vector_in[j]*Ax[i];
     }
   }
 }
