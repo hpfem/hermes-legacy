33e3f2c113a3aee2132922a79fbd080c6f95039d
Ma Zhonghua <mazhonghua83@gmail.com>
2011-06-14 20:43:46 +0800

    H2D: splitting of definitions.cpp for regular example acoustics/apartment.

--- hermes2d/examples/acoustics/apartment/definitions.cpp
+++ hermes2d/examples/acoustics/apartment/definitions.cpp
@@ -1,22 +1,17 @@
-#include "hermes2d.h"
+#include "definitions.h"
 
-/* Weak forms */
-
-class CustomWeakFormAcoustics : public WeakForm
+CustomWeakFormAcoustics::CustomWeakFormAcoustics(std::string bdy_newton, double rho, double sound_speed, double omega) : WeakForm(1) 
 {
-public:
-  CustomWeakFormAcoustics(std::string bdy_newton, double rho, double sound_speed, double omega) : WeakForm(1) 
-  {
-    scalar ii =  cplx(0.0, 1.0);
+  scalar ii =  cplx(0.0, 1.0);
 
-    // Jacobian.
-    add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion(0, 0, HERMES_ANY, new HermesFunction(1.0/rho), HERMES_SYM));
-    add_matrix_form(new WeakFormsH1::DefaultMatrixFormVol(0, 0, HERMES_ANY, new HermesFunction(-sqr(omega) / rho / sqr(sound_speed)), HERMES_SYM));
-    add_matrix_form_surf(new WeakFormsH1::DefaultMatrixFormSurf(0, 0, bdy_newton, new HermesFunction(-ii * omega / rho / sound_speed)));
+  // Jacobian.
+  add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion(0, 0, HERMES_ANY, new HermesFunction(1.0/rho), HERMES_SYM));
+  add_matrix_form(new WeakFormsH1::DefaultMatrixFormVol(0, 0, HERMES_ANY, new HermesFunction(-sqr(omega) / rho / sqr(sound_speed)), HERMES_SYM));
+  add_matrix_form_surf(new WeakFormsH1::DefaultMatrixFormSurf(0, 0, bdy_newton, new HermesFunction(-ii * omega / rho / sound_speed)));
 
-    // Residual.
-    add_vector_form(new WeakFormsH1::DefaultResidualDiffusion(0, HERMES_ANY, new HermesFunction(1.0/rho)));
-    add_vector_form(new WeakFormsH1::DefaultResidualVol(0, HERMES_ANY, new HermesFunction(-sqr(omega) / rho / sqr(sound_speed))));
-    add_vector_form_surf(new WeakFormsH1::DefaultResidualSurf(0, bdy_newton, new HermesFunction(-ii * omega / rho / sound_speed)));
-  };
-};
+  // Residual.
+  add_vector_form(new WeakFormsH1::DefaultResidualDiffusion(0, HERMES_ANY, new HermesFunction(1.0/rho)));
+  add_vector_form(new WeakFormsH1::DefaultResidualVol(0, HERMES_ANY, new HermesFunction(-sqr(omega) / rho / sqr(sound_speed))));
+  add_vector_form_surf(new WeakFormsH1::DefaultResidualSurf(0, bdy_newton, new HermesFunction(-ii * omega / rho / sound_speed)));
+}
+
---
+++ hermes2d/examples/acoustics/apartment/definitions.h
@@ -0,0 +1,9 @@
+#include "hermes2d.h"
+
+/* Weak forms */
+
+class CustomWeakFormAcoustics : public WeakForm
+{
+public:
+  CustomWeakFormAcoustics(std::string bdy_newton, double rho, double sound_speed, double omega);
+};
