60edce04b99df63f7d511017cd778750c303fede
Pavel Solin <solin.pavel@gmail.com>
2011-07-10 16:55:37 -0400

    Fixing some meshes.

--- hermes2d/benchmarks-general/moving-front-space-adapt/main.cpp
+++ hermes2d/benchmarks-general/moving-front-space-adapt/main.cpp
@@ -21,7 +21,7 @@ using namespace RefinementSelectors;
 //  The following parameters can be changed:
 
 const int INIT_REF_NUM = 1;                       // Number of initial uniform mesh refinements.
-const int P_INIT = 1;                             // Initial polynomial degree of all mesh elements.
+const int P_INIT = 2;                             // Initial polynomial degree of all mesh elements.
 const double time_step = 0.1;                     // Time step. 
 const double T_FINAL = 10.0;                      // Time interval length.
 
