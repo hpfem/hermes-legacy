231c99afb07cf5833682796b3d75d7eb99e2465f
Ma Zhonghua <mazhonghua83@gmail.com>
2011-07-11 18:15:12 +0800

    H2D: updating example microwave-oven to nonlinear format.      updating weakforms of example microwave-oven. However, Newton's iteration failed in this example.

--- hermes2d/examples/maxwell/microwave-oven/definitions.cpp
+++ hermes2d/examples/maxwell/microwave-oven/definitions.cpp
@@ -1,24 +1,26 @@
 #include "definitions.h"
 
-template<typename Scalar, typename Real>
-Scalar CustomMatrixForm::matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, 
+template<typename Real, typename Scalar>
+Scalar CustomMatrixForm::matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u,
                                      Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const 
 {
   cplx ikappa = cplx(0.0, kappa);
   Scalar result1 = 0;
   Scalar result2 = 0;
+  Scalar result3 = 0;
   for (int i = 0; i < n; i++)
     result1 += wt[i] * gamma(e->elem_marker, e->x[i], e->y[i]) * (u->val0[i] * conj(v->val0[i]) + u->val1[i] * conj(v->val1[i]));
   for (int i = 0; i < n; i++)
     result2 += wt[i] * er(e->elem_marker, e->x[i], e->y[i]) * (u->val0[i] * conj(v->val0[i]) + u->val1[i] * conj(v->val1[i]));
-  return 1.0/mu_r * int_curl_e_curl_f<Real, Scalar>(n, wt, u, v) -
-         ikappa * sqrt(mu_0 / e_0) * result1 - sqr(kappa) * result2;
+  for (int i = 0; i < n; i++)
+    result3 += wt[i] * (u->curl[i] * conj(v->curl[i]));
+  return 1.0/mu_r * result3 - ikappa * sqrt(mu_0 / e_0) * result1 - sqr(kappa) * result2;
 }
 
 scalar CustomMatrixForm::value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *u, 
                                Func<double> *v, Geom<double> *e, ExtData<scalar> *ext) const 
 {
-  return matrix_form<scalar, double>(n, wt, u_ext, u, v, e, ext);
+  return matrix_form<double, scalar>(n, wt, u_ext, u, v, e, ext);
 }
 
 Ord CustomMatrixForm::ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v, 
@@ -71,9 +73,79 @@ bool CustomMatrixForm::in_load(double x, double y) const
 }
 
 
+template<typename Real, typename Scalar>
+Scalar CustomResidualForm::vector_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v,
+                                       Geom<Real> *e, ExtData<Scalar> *ext) const 
+{
+  cplx ikappa = cplx(0.0, kappa);
+  Scalar result1 = 0;
+  Scalar result2 = 0;
+  Scalar result3 = 0;
+  for (int i = 0; i < n; i++)
+    result1 += wt[i] * gamma(e->elem_marker, e->x[i], e->y[i]) * (u_ext[0]->val0[i] * conj(v->val0[i]) + u_ext[0]->val1[i] * conj(v->val1[i]));
+  for (int i = 0; i < n; i++)
+    result2 += wt[i] * er(e->elem_marker, e->x[i], e->y[i]) * (u_ext[0]->val0[i] * conj(v->val0[i]) + u_ext[0]->val1[i] * conj(v->val1[i]));
+  for (int i = 0; i < n; i++)
+    result3 += wt[i] * (u_ext[0]->curl[i] * conj(v->curl[i]));
+  return 1.0/mu_r * result3 - ikappa * sqrt(mu_0 / e_0) * result1 - sqr(kappa) * result2;
+}
+
+scalar CustomResidualForm::value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *v, Geom<double> *e, ExtData<scalar> *ext) const 
+{
+  return vector_form<double, scalar>(n, wt, u_ext, v, e, ext);
+}
+
+Ord CustomResidualForm::ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext) const 
+{
+  return vector_form<Ord, Ord>(n, wt, u_ext, v, e, ext);
+}
+
+double CustomResidualForm::gamma(int marker, double x, double y) const
+{
+  if (align_mesh && marker == 1) return 0.03;
+  if (!align_mesh && in_load(x,y)) 
+  {
+    double cx = -0.152994121;  double cy =  0.030598824;
+    double r = sqrt(sqr(cx - x) + sqr(cy - y));
+    return (0.03 + 1)/2.0 - (0.03 - 1) * atan(10.0*(r -  0.043273273)) / M_PI;
+  }
+  return 0.0;
+}
+
+double CustomResidualForm::gamma(int marker, Ord x, Ord y) const
+{  
+  return 0.0; 
+}
+
+double CustomResidualForm::er(int marker, double x, double y) const
+{
+  if (align_mesh && marker == 1) return 7.5;
+  if (!align_mesh && in_load(x,y)) 
+  {
+    double cx = -0.152994121;  double cy =  0.030598824;
+    double r = sqrt(sqr(cx - x) + sqr(cy - y));
+    return (7.5 + 1)/2.0 - (7.5 - 1) * atan(10.0*(r -  0.043273273)) / M_PI;
+  }
+  return 1.0;
+}
+
+double CustomResidualForm::er(int marker, Ord x, Ord y) const
+{  
+  return 1.0; 
+}
+
+bool CustomResidualForm::in_load(double x, double y) const
+{
+  double cx = -0.152994121;
+  double cy =  0.030598824;
+  double r = 0.043273273;
+  if (sqr(cx - x) + sqr(cy - y) < sqr(r)) return true;
+  else return false;
+}
+
 template<typename Scalar, typename Real>
 Scalar CustomVectorFormSurf::vector_form_surf(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, 
-                          Geom<Real> *e, ExtData<Scalar> *ext) const 
+                                              Geom<Real> *e, ExtData<Scalar> *ext) const 
 {
   cplx ii = cplx(0.0, 1.0);
   return ii * omega * J * int_v1<Real, Scalar>(n, wt, v); // just second component of v, since J = (0, J)
@@ -91,10 +163,17 @@ Ord CustomVectorFormSurf::ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *
   return vector_form_surf<Ord, Ord>(n, wt, u_ext, v, e, ext);
 }
 
+
 CustomWeakForm::CustomWeakForm(double e_0, double mu_0, double mu_r, double kappa, double omega, 
                                double J, bool align_mesh) : WeakForm(1) 
 {
-  add_matrix_form(new CustomMatrixForm(0, 0, e_0, mu_0, mu_r, kappa, align_mesh));
+  // Jacobian forms - volumetric.
+  add_matrix_form(new CustomMatrixForm(0, 0, e_0, mu_0, mu_r, kappa, omega, J, align_mesh));
+
+  // Residual forms - volumetric.
+  add_vector_form(new CustomResidualForm(0, e_0, mu_0, mu_r, kappa, omega, J, align_mesh));
+
+  // Residual forms - surface.
   add_vector_form_surf(new CustomVectorFormSurf(omega, J));
 }
 
--- hermes2d/examples/maxwell/microwave-oven/definitions.h
+++ hermes2d/examples/maxwell/microwave-oven/definitions.h
@@ -2,25 +2,26 @@
 #include "weakform/weakform.h"
 #include "integrals/hcurl.h"
 #include "boundaryconditions/essential_bcs.h"
+#include "weakform_library/weakforms_hcurl.h"
 
 /* Weak forms */
 
 class CustomMatrixForm : public WeakForm::MatrixFormVol
 {
 public:
-  CustomMatrixForm(int i, int j, double e_0, double mu_0, double mu_r, double kappa, bool align_mesh) 
+  CustomMatrixForm(int i, int j, double e_0, double mu_0, double mu_r, double kappa, double omega, double J, bool align_mesh) 
         : WeakForm::MatrixFormVol(i, j, HERMES_ANY, HERMES_SYM), e_0(e_0), mu_0(mu_0), 
-          mu_r(mu_r), kappa(kappa), align_mesh(align_mesh) {};
+          mu_r(mu_r), kappa(kappa), omega(omega), J(J), align_mesh(align_mesh) {};
 
-  template<typename Scalar, typename Real>
-  Scalar matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, 
+  template<typename Real, typename Scalar>
+  Scalar matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u,
                      Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const;
 
   virtual scalar value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *u, 
                        Func<double> *v, Geom<double> *e, ExtData<scalar> *ext) const;
 
-  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v, 
-                  Geom<Ord> *e, ExtData<Ord> *ext) const;
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, 
+                  Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext) const;
 
   // Gamma as a function of x, y.
   double gamma(int marker, double x, double y) const;
@@ -36,8 +37,45 @@ public:
   bool in_load(double x, double y) const;
 
   private:
-  double e_0, mu_0, mu_r, kappa, align_mesh;
+  double e_0, mu_0, mu_r, kappa, omega, J;
+  bool align_mesh;
 };
+
+class CustomResidualForm : public WeakForm::VectorFormVol
+{
+public:
+  CustomResidualForm(int i, double e_0, double mu_0, double mu_r, double kappa, double omega, double J, bool align_mesh) 
+        : WeakForm::VectorFormVol(i), e_0(e_0), mu_0(mu_0), 
+          mu_r(mu_r), kappa(kappa), omega(omega), J(J), align_mesh(align_mesh) {};
+
+  template<typename Real, typename Scalar>
+  Scalar vector_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v,
+                     Geom<Real> *e, ExtData<Scalar> *ext) const;
+
+  virtual scalar value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *v, Geom<double> *e,
+                       ExtData<scalar> *ext) const;
+
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, 
+                  ExtData<Ord> *ext) const;
+
+  // Gamma as a function of x, y.
+  double gamma(int marker, double x, double y) const;
+
+  double gamma(int marker, Ord x, Ord y) const;
+
+  // Relative permittivity as a function of x, y.
+  double er(int marker, double x, double y) const;
+
+  double er(int marker, Ord x, Ord y) const;
+
+  // Geometry of the load.
+  bool in_load(double x, double y) const;
+
+  private:
+  double e_0, mu_0, mu_r, kappa, omega, J;
+  bool align_mesh;
+};
+
 
 class CustomVectorFormSurf : public WeakForm::VectorFormSurf
 {
