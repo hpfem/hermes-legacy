06a1c8c12ad34f650f1ac66daec3d8838d2c77d2
Pavel Solin <solin.pavel@gmail.com>
2011-07-24 11:03:22 -0400

    New benchmark moving-front-non-adapt added.

---
+++ hermes2d/benchmarks-general/moving-front-non-adapt/plot_error_history.py
@@ -0,0 +1,22 @@
+# import libraries
+import numpy, pylab
+from pylab import *
+
+# plot DOF convergence graph
+pylab.title("Error history")
+pylab.xlabel("Physical time")
+pylab.ylabel("Error")
+data = numpy.loadtxt("err_exact_history.dat")
+x = data[:, 0]
+y = data[:, 1]
+semilogy(x, y, "-", label="exact error")
+
+legend()
+
+# initialize new window
+#pylab.figure()
+
+
+
+# finalize
+show()
